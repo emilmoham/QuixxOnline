afeabcd11cfc0fcfd0373c29e0a858f48cf6054f
Emil Mohammed <emilmoham@gmail.com>
2021-06-03 01:11:27 +0000

    Now uses Scoreboard component

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ScoreCard/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ScoreCard from './components/ScoreCard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ScoreCard
   }
 }
 </script>

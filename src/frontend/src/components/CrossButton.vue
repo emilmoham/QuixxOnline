dd14cf979e7044769cdff0945c498c994b9ac9be
Emil Mohammed <emilmoham@gmail.com>
2021-06-03 01:08:13 +0000

    Basic cross field component for scoreboard

---
+++ src/frontend/src/components/CrossButton.vue
@@ -0,0 +1,36 @@
+<template>
+    <button class="colorButton " :class="{ selected: isSelected }" v-on:mousedown="toggleCross()" >{{ label }}</button>
+</template>
+
+<script>
+export default {
+  name: "CrossButton",
+  props: {
+    label: Number
+  },
+  data: function () {
+    return {
+      isSelected: false
+    }
+  },
+  methods: {
+    toggleCross: function() {
+      this.isSelected = !this.isSelected
+    }
+  }
+}
+</script>
+
+<style scoped>
+button {
+  font-size: large;
+	text-align: center;
+	border: 0;
+	border-radius: 15%;
+	
+	width: 40px;
+	height: 40px;
+	
+	margin: 0px 2px 0px 2px;
+}
+</style>
